9da410f3cd0a238938047ec677071b8a2674e1eb
Lester Young <lyoung070789@gmail.com>
2020-02-13 19:08:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min
       min_key = key
-      min=value
+      min = value
     else
       min = min 
       min_key = min_key
