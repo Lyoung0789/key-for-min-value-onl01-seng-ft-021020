1fd7680ed2bc014ca673ccfdedf95fa02b836dd3
Lester Young <lyoung070789@gmail.com>
2020-02-13 19:11:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
+#stored the first hash in variables min_key and min.
+#.each method is called to compare each hash value and returns the lowest key 
+#if/else statement is used. and stores the lowest value and key in min and min_key respectively
 
 def key_for_min_value(name_hash)
   min_key, min = name_hash.first
