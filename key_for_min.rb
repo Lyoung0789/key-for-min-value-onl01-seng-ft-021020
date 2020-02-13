13d92f6a127c426fd4b81cc231bd96115c52fbd3
Lester Young <lyoung070789@gmail.com>
2020-02-13 18:50:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = ""
+  name_hash.collect do |key, value|
+    if min > value
+      min=key
+    else 
+      min < value 
+      min = min
+    end 
+  end
 end
